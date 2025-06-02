bdd2a236e6bea7b7aa6ac7664c57d20c5365efb3
Virginia <mvirginiamontoya@yahoo.com.ar>
2025-06-02 12:54:01 -0300

    cambio url redireccion meli

--- src/controllers/mercadoLibre/mercadoLibreAuthController.js
+++ src/controllers/mercadoLibre/mercadoLibreAuthController.js
@@ -7,9 +7,9 @@ const { SocialMediaActive } = require("../../db");
 const mercadoLibreAuthController = {
   getAuthUrl: () => {
     const clientId = process.env.ML_CLIENT_ID;
-    const redirectUri = process.env.ML_REDIRECT_URI;
-   // return `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
-   return `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=${clientId}&redirect_uri=https://electrica-mosconi-frontend-ebon.vercel.app/MeLi_auth`;
+    const redirectUri =
+      process.env.ML_REDIRECT_URI || "https://oneinbox.vercel.app/MeLi_auth"; // URL a donde Mercado Libre redirige después de la autenticación.
+    return `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
   },
 
   getAccessToken: async (authorizationCode) => {
@@ -36,31 +36,41 @@ const mercadoLibreAuthController = {
         }
       );
 
-      const { access_token, refresh_token, expires_in = 21600, user_id } = response.data;
+      const {
+        access_token,
+        refresh_token,
+        expires_in = 21600,
+        user_id,
+      } = response.data;
       console.log("MELI-AUTH: Tokens obtenidos:", response.data);
-        //  access_token → Token que permite hacer solicitudes a la API de Mercado Libre.
-        // refresh_token → Token que permite renovar el access_token cuando expire.
-        // expires_in → Tiempo en segundos hasta que expire el access_token (por defecto, 6 horas).
-        // user_id → ID del usuario en Mercado Libre.
+      //  access_token → Token que permite hacer solicitudes a la API de Mercado Libre.
+      // refresh_token → Token que permite renovar el access_token cuando expire.
+      // expires_in → Tiempo en segundos hasta que expire el access_token (por defecto, 6 horas).
+      // user_id → ID del usuario en Mercado Libre.
 
-     // Validar y calcular expirationDate
-     if (typeof expires_in !== "number" || expires_in <= 0) {
-      console.warn("MELI-AUTH: El campo expires_in es inválido. Se usará el valor predeterminado de 6 horas.");
-    }
+      // Validar y calcular expirationDate
+      if (typeof expires_in !== "number" || expires_in <= 0) {
+        console.warn(
+          "MELI-AUTH: El campo expires_in es inválido. Se usará el valor predeterminado de 6 horas."
+        );
+      }
 
-    const expirationDate = new Date(Date.now() + expires_in * 1000);
-    console.log("MELI-AUTH: Fecha de expiración calculada:", expirationDate);
-    
+      const expirationDate = new Date(Date.now() + expires_in * 1000);
+      console.log("MELI-AUTH: Fecha de expiración calculada:", expirationDate);
+
       return {
         accessToken: access_token,
         refreshToken: refresh_token,
         expires_in,
         expirationDate,
         authorizationCode,
-        userId: user_id,// Incluimos el user id para obtenerlo y usarlo como dataUser
+        userId: user_id, // Incluimos el user id para obtenerlo y usarlo como dataUser
       };
     } catch (error) {
-      console.error("MELI-AUTH C: Error al obtener el token de acceso:", error.response?.data || error.message);
+      console.error(
+        "MELI-AUTH C: Error al obtener el token de acceso:",
+        error.response?.data || error.message
+      );
       throw new Error(
         "MELI-AUTH: No se pudo obtener el token de acceso. Verifica el código de autorización y otros parámetros."
       );
@@ -76,7 +86,7 @@ const mercadoLibreAuthController = {
       const response = await axios.post(
         "https://api.mercadolibre.com/oauth/token",
         querystring.stringify({
-          grant_type: "refresh_token",//ndica que se usa un refresh_token para obtener un nuevo access_token
+          grant_type: "refresh_token", //ndica que se usa un refresh_token para obtener un nuevo access_token
           client_id: clientId,
           client_secret: clientSecret,
           refresh_token: refreshToken,
@@ -86,7 +96,11 @@ const mercadoLibreAuthController = {
         }
       );
 
-      const { access_token, refresh_token: newRefreshToken, expires_in } = response.data;
+      const {
+        access_token,
+        refresh_token: newRefreshToken,
+        expires_in,
+      } = response.data;
       console.log("MELI-AUTH: Nuevo token de acceso obtenido:", response.data);
 
       const newExpirationDate = new Date(Date.now() + expires_in * 1000);
@@ -94,10 +108,17 @@ const mercadoLibreAuthController = {
       return { accessToken: access_token, newRefreshToken, newExpirationDate };
     } catch (error) {
       if (error.response && error.response.status === 401) {
-        console.error("MELI-AUTH: El refresh token ha expirado o es inválido. Se requiere reautenticación.");
-        throw new Error("MELI-AUTH: El refresh token ha expirado o es inválido. Realiza la autenticación nuevamente.");
+        console.error(
+          "MELI-AUTH: El refresh token ha expirado o es inválido. Se requiere reautenticación."
+        );
+        throw new Error(
+          "MELI-AUTH: El refresh token ha expirado o es inválido. Realiza la autenticación nuevamente."
+        );
       }
-      console.error("MELI-AUTH: Error al renovar el token de acceso:", error.message);
+      console.error(
+        "MELI-AUTH: Error al renovar el token de acceso:",
+        error.message
+      );
       throw error;
     }
   },
@@ -105,19 +126,29 @@ const mercadoLibreAuthController = {
   checkAndRefreshToken: async (userId) => {
     try {
       const socialMediaActive = await SocialMediaActive.findOne({
-        where: { dataUser: "Mercado Libre", userId: userId, socialMediaId: 5, active: true },
+        where: {
+          dataUser: "Mercado Libre",
+          userId: userId,
+          socialMediaId: 5,
+          active: true,
+        },
       });
 
       if (!socialMediaActive) {
-        throw new Error("MELI-AUTH: No se encontró un token activo en la base de datos.");
+        throw new Error(
+          "MELI-AUTH: No se encontró un token activo en la base de datos."
+        );
       }
 
       const { accessToken, refreshToken, expirationDate } = socialMediaActive;
 
       if (new Date(expirationDate).getTime() - Date.now() < 3600000) {
         console.log("MELI-AUTH: El token está por expirar. Renovando...");
-        const { accessToken: newAccessToken, newRefreshToken, newExpirationDate } =
-          await mercadoLibreAuthController.refreshAccessToken(refreshToken);
+        const {
+          accessToken: newAccessToken,
+          newRefreshToken,
+          newExpirationDate,
+        } = await mercadoLibreAuthController.refreshAccessToken(refreshToken);
 
         await socialMediaActive.update({
           accessToken: newAccessToken,
@@ -125,7 +156,9 @@ const mercadoLibreAuthController = {
           expirationDate: newExpirationDate,
         });
 
-        console.log("MELI-AUTH: Token renovado y guardado en la base de datos.");
+        console.log(
+          "MELI-AUTH: Token renovado y guardado en la base de datos."
+        );
         return newAccessToken;
       }
 
@@ -137,4 +170,4 @@ const mercadoLibreAuthController = {
   },
 };
 
-module.exports =  mercadoLibreAuthController ;
+module.exports = mercadoLibreAuthController;
